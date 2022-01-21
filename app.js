d1cd4c1bccb5ab92f8bafb426220bec17c1a5786
ARUN MOHANAN <kriactivedesigns@gmail.com>
2022-01-21 16:39:13 +0530

    Updated app.js with new dummy User

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const errorController = require('./controllers/error')
 const app = express();
 
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -25,9 +27,26 @@ app.use(shopRoutes);
 // Handling all other requests which are not handled by the other roters
 app.use(errorController.get404);
 
+// Before relate the models
+Product.belongsTo(User, {
+    constraints: true,
+    onDelete: 'CASCADE'
+})
+User.hasMany(Product)
+
 sequelize
     .sync()
     .then(result => {
+        return User.findByPk(1)
+    })
+    .then(user => {
+        if(!user) {
+            User.create({ name: "ARUN MOHANAN", email: 'arun@test.com' })
+        }
+        return user
+    })
+    .then(user => {
+        console.log(user)
         app.listen(3000, () => {
             console.log("Application runs at localhost:3000");
         });
