e75621b20ea301d71cf2e7b72dacc95f460f23b4
ARUN MOHANAN <kriactivedesigns@gmail.com>
2022-01-21 17:01:23 +0530

    Cart Models added and Relations defined in app.js

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -43,9 +45,14 @@ Product.belongsTo(User, {
     constraints: true,
     onDelete: 'CASCADE'
 })
-User.hasMany(Product)
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
+    //.sync({ force: true })
     .sync()
     .then(result => {
         return User.findByPk(1)
